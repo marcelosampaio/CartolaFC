e287a567bbd1b4d0a34ce51e7d035842eb1d8540
Marcelo Sampaio <marcelo@artlogica.com.br>
2017-12-14 20:50:25 -0200

    working on UI 🖼

---
+++ CartolaFC/Controller/MenuController.swift
@@ -0,0 +1,21 @@
+//
+//  MenuController.swift
+//  CartolaFC
+//
+//  Created by Marcelo on 12/14/17.
+//  Copyright © 2017 Marcelo Sampaio. All rights reserved.
+//
+
+import UIKit
+
+class MenuController: UITableViewController {
+
+    
+    // MARK: - View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
