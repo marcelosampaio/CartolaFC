4d0d26ac1b65e9ac39e37fadfc676a6fb507b92b
Marcelo Sampaio <marcelo@artlogica.com.br>
2017-12-16 00:58:13 -0200

    preparing to order top 10 results

--- CartolaFC/Controller/TopController.swift
+++ CartolaFC/Controller/TopController.swift
@@ -14,12 +14,14 @@ class TopController: UITableViewController {
     // MARK: - Properties
     var clubes = [Clube]()
     var sortedClubes = [Clube]()
-
+    var organizedResult = [Clube]()
+    
     // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         appearance()
-        sortedClubes = clubes.sorted(by: { $0.posicao! < $1.posicao! })
+        organizer(asc: true)
+        
         
     }
 
@@ -27,21 +29,42 @@ class TopController: UITableViewController {
     private func appearance() {
         navigationItem.title = "Top 10"
         tableView.backgroundColor = UIColor.backgroundColor
-        
     }
 
+    // MARK: - Helper
+    private func organizer(asc: Bool) {
+        sortedClubes = clubes.sorted(by: { $0.posicao! < $1.posicao! })
+        
+        
+        // just 10 occurrences
+        organizedResult.removeAll()
+        if asc {
+            for i in 0..<sortedClubes.count - 1 {
+                if i < 10 {
+                    organizedResult.append(sortedClubes[i])
+                }
+            }
+        } else {
+            for i in (0..<sortedClubes.count).reversed() {
+                if i < 10 {
+                    organizedResult.append(sortedClubes[i])
+                }
+            }
+        }
+    }
+    
     // MARK: - Table View DataSource and Delegate
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return sortedClubes.count
+        return organizedResult.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! TopTeamsCell
-        let clube = sortedClubes[indexPath.row]
+        let clube = organizedResult[indexPath.row]
         cell.name.text = clube.nome
         cell.position.text = String(clube.posicao!) + "º"
         
